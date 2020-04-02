400f69f44478f9cf72670ab964b693f6ecaf4546
dev-yongseok <yys0863@naver.com>
2020-04-03 03:51:20 +0900

    Auth 기능 만들기

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser =require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
-
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -22,7 +22,7 @@ mongoose.connect(config.mongoURI,{
 
 app.get('/', (req, res) => res.send('노드 리액트 공부하기!'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     
     // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 
     // 그 정보를 데이터베이스에 넣어줌
@@ -37,7 +37,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({email : req.body.email}, (err, user) => {
         console.log("find email...")
@@ -71,5 +71,22 @@ app.post('/login', (req, res) => {
         })
     })
 })
+
+app.get('api/users.auth', auth , (req, res) => {
+
+    // 여기 까지 미들웨어를 통과해 왔다는 얘기는 authentication이 true 라는 뜻.
+    res.status(200).json({
+        _id : req.user._id,
+        isAdmin : req.user.role == 0 ? false : true,
+        isAuth : true,
+        email : req.user.email,
+        name : req.user.name,
+        lastname :req.user.lastname,
+        role :req.user.role,
+        image : req.user.image
+    })
+
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const { User } = require('../models/User');
+
+let auth = (req, res , next) =>{
+    // 인증 처리를 하는 곳
+
+    // Client Cookie에서 Token을 가져온다.
+    let token = req.cookies.x_auth;
+
+
+    // 토큰을 Decode 한 후 유저를 찾는다.
+    User.findByToken(token, (err, user) => {
+        if(err) throw err;
+        if(!user) return res.json({ isAuth : false , error : ture})
+
+        req.token = token;
+        req.user = user;
+        next();
+    })
+}
+
+module.exports = { auth };
