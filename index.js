a2f7d0f4beb0fca3c432a1a709e5c9b0b9e02cf0
dev-yongseok <yys0863@naver.com>
2020-04-02 16:48:56 +0900

    Bcrypt 비밀번호 암호화 -2, 로그인 Route 및 토큰 생성

--- index.js
+++ index.js
@@ -36,4 +36,25 @@ app.post('/register', (req, res) => {
     })
 })
 
+app.post('/login', (req, res) => {
+    // 1. 요청된 이메일을 데이터베이스에서 찾는다.
+    User.findOne({email:req.body.email}, (err, userInfo) => {
+        if(!userInfo){
+            return res.json({
+                loginSucess : false,
+                message : "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+    // 2. 요청된 이메일이 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password , (err, isMatch) => {
+            if(!isMatch)
+            return res.json({ loginSucess : false, message : "비밀번호가 틀렸습니다." })
+
+            // 3. 비밀번호 까지 같다면 토큰 생성
+            user.generateToken((err, user) => {
+                
+        })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
