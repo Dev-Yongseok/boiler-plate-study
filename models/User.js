711522c0f15d70df0b4616fc4184c637b2843678
dev-yongseok <yys0863@naver.com>
2020-04-02 14:04:25 +0900

    Bcrypt 비밀번호 암호화 - 1

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlainTextPassword = ' s0 / \ / \ P 4 $$ w0rD ' ;
+const someOtherPlaintextPassword = ' not_bacon ' ;   
 
 const userSchema = mongoose.Schema({
     name : {
@@ -28,6 +32,24 @@ const userSchema = mongoose.Schema({
     },
     tokenExp:{
         type : Number
+    }
+})
+
+userSchema.pre('save', function(next){
+    var user = this;
+    
+    // 비밀번호가 modified 되면
+    if(user.isModified('password')){
+    // 비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function(err, salt){
+        if(err) return next(err)
+
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err)
+            user.password = hash
+            next()
+        });
+    });
     }
 })
 
