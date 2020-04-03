3c37326400ca2065a486554d63cb9a2e460e7b79
dev-yongseok <yys0863@naver.com>
2020-04-04 00:38:01 +0900

    node, react 연동하기 테스트 = CORS이슈 발생

--- client/src/components/views/LangdingPage/LandingPage.js
+++ client/src/components/views/LangdingPage/LandingPage.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React , { useEffect} from 'react'
+import axios from 'axios';
 
 function LandingPage() {
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/hello')
+        .then( response => console.log(response.data))
+    }, [])
+
     return (
         <div>
-            LandingPage
+            LandingPage 랜딩 페이지 입니당.
         </div>
     )
 }
